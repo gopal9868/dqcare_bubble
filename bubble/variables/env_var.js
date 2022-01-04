f3848ceb4d275fd599c883ed145c5bf83656237e
gopal9868 <gopal.kumar8@gmail.com>
2022-01-03 20:44:58 -0600

    modifying the UI look

--- bubble/variables/env_var.js
+++ bubble/variables/env_var.js
@@ -1,4 +1,4 @@
-const raindropserver='http://ec2-54-84-158-13.compute-1.amazonaws.com:8000/'
+const raindropserver='http://127.0.0.1:8000/'
 export { raindropserver };
 const supporteddb=['Mysql','Redshift','Bigquery','Postgres','Athena','Oracle','Teradata','DB2']
 export {supporteddb};
